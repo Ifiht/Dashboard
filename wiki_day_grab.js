a84e38abcba1c0907b397eaa7d314f98e18473a3
Peter Pan <peter@never.lan>
2022-01-30 19:09:22 -0500

    update wiki grab res

--- wiki_day_grab.js
+++ wiki_day_grab.js
@@ -3,7 +3,7 @@ const Pageres = require('pageres');
 (async () => {
     await new Pageres({delay: 2})
         .src('https://en.wikipedia.org/wiki/Main_Page',
-         ['1490x1024'], {crop: true, filename: 'otd_note-latest',
+         ['1590x980'], {crop: true, filename: 'otd_note-latest', hide: ['#mp-left'],
          selector: "#mp-right"})
         .dest(__dirname + "/screenshots")
         .run();
